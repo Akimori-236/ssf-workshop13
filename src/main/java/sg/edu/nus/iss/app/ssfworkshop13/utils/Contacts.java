e8135f5f3f9bf01cd53760ac667bfbdbc422963a
Wee Seng <wee.bscl@gmail.com>
2023-01-09 22:40:52 +0800

    working list of contacts

--- src/main/java/sg/edu/nus/iss/app/ssfworkshop13/utils/Contacts.java
+++ src/main/java/sg/edu/nus/iss/app/ssfworkshop13/utils/Contacts.java
@@ -9,6 +9,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -22,6 +23,32 @@ import sg.edu.nus.iss.app.ssfworkshop13.models.Contact;
 
 @Component("contacts")
 public class Contacts {
+    // loading charset (retrieving need UTF but saving dont need UTF)
+    public static final Charset charset = Charset.forName("UTF-8");
+    public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    // get arguments from the command line
+    private String getDataDir(ApplicationArguments appArgs, String defaultDataDir) {
+        String dataDirResult = null;
+        List<String> optValues = null;
+
+        // currently appArgs will have 1 key:value pair if user inputs cmd line argument
+        // kind of appArgs.keyset()
+        Set<String> opsNames = appArgs.getOptionNames();
+        String[] optNamesArr = opsNames.toArray(new String[opsNames.size()]);
+
+        // if there is a argument, get the value
+        if (optNamesArr.length > 0) {
+            // do we need to check the argument name???
+            optValues = appArgs.getOptionValues(optNamesArr[0]);
+            // optValuesArr = optValues.toArray(new String[optValues.size()]);
+            dataDirResult = optValues.get(0);
+        } else {
+            dataDirResult = defaultDataDir;
+        }
+
+        return dataDirResult;
+    }
 
     // write the file
     public void saveContact(Contact contact, Model model, ApplicationArguments appArgs, String defaultDataDir) {
@@ -49,36 +76,11 @@ public class Contacts {
         }
     }
 
-    // get arguments from the command line
-    private String getDataDir(ApplicationArguments appArgs, String defaultDataDir) {
-        String dataDirResult = null;
-        List<String> optValues = null;
-
-        // currently appArgs will have 1 key:value pair if user inputs cmd line argument
-        // kind of appArgs.keyset()
-        Set<String> opsNames = appArgs.getOptionNames();
-        String[] optNamesArr = opsNames.toArray(new String[opsNames.size()]);
-
-        // if there is a argument, get the value
-        if (optNamesArr.length > 0) {
-            // do we need to check the argument name???
-            optValues = appArgs.getOptionValues(optNamesArr[0]);
-            // optValuesArr = optValues.toArray(new String[optValues.size()]);
-            dataDirResult = optValues.get(0);
-        } else {
-            dataDirResult = defaultDataDir;
-        }
-
-        return dataDirResult;
-    }
-
     // read the file
     public void getContactByID(Model model, String contactID, ApplicationArguments appArgs, String defaultDataDir) {
         Contact c = new Contact();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd");
         Path filepath = new File(getDataDir(appArgs, defaultDataDir) + "/" + contactID).toPath();
-        // loading charset (retrieving need UTF but saving dont need UTF)
-        Charset charset = Charset.forName("UTF-8");
+
         try {
             // read all the lines in the file with UTF-8
             List<String> dataLines = Files.readAllLines(filepath, charset);
@@ -100,5 +102,34 @@ public class Contacts {
         }
         // send c to page view
         model.addAttribute("contact", c);
+    }
+
+    // get all contacts
+    public void getAllContacts(Model model, ApplicationArguments appArgs, String defaultDataDir) {
+        List<Contact> contactList = new LinkedList<>();
+
+        // get list of files
+        File directory = new File(getDataDir(appArgs, defaultDataDir));
+        File[] fileList = directory.listFiles();
+
+        // read every file and create corresponding Contact object
+        for (File f : fileList) {
+            Path filepath = f.toPath();
+            Contact c = new Contact();
+            try {
+                List<String> dataLines = Files.readAllLines(filepath, charset);
+                c.setId(filepath.getFileName().toString());
+                c.setName(dataLines.get(0));
+                c.setEmail(dataLines.get(1));
+                c.setPhoneNumber(dataLines.get(2));
+                LocalDate dob = LocalDate.parse(dataLines.get(3), formatter);
+                c.setDob(dob);
+                contactList.add(c);
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+        // send contact list to page view
+        model.addAttribute("contactList", contactList);
     }
 }
